0cc88d7a9a4885dbbf55205c611328a2cb9f746a
IlNat <ilanatalcenko18@gmail.com>
2022-07-05 15:18:48 +0500

    Add code.

--- Field.cpp
+++ Field.cpp
@@ -94,5 +94,116 @@ void print_field(char field[10][10], int a)
 
 bool Field::isAvailable(int XCord, int YCord, int amountOfUnits, bool isVertical)
 {
+	int operationNumber = 0;
+	if (!isVertical)
+	{
+		if (XCord == 0 && YCord == 0)
+			operationNumber = 1;
+		else if (XCord == 0 && YCord > 0 && YCord < size - amountOfUnits)
+			operationNumber = 2;
+		else if (XCord > 0 && XCord < 9 && YCord == 0)
+			operationNumber = 3;
+		else if (XCord > 0 && XCord < 9 && YCord > 0 && YCord < size - amountOfUnits)
+			operationNumber = 4;
+		else if (XCord == 9 && YCord == 0)
+			operationNumber = 5;
+		else if (XCord == 9 && YCord > 0 && YCord < size - amountOfUnits)
+			operationNumber = 6;
+		else 
+			return false;
+	}
+	else
+	{
+		if (XCord == 0 && YCord == 0)
+			operationNumber = 7;
+		else if (YCord == 0 && XCord > 0 && XCord < size - amountOfUnits)
+			operationNumber = 8;
+		else if (YCord > 0 && YCord < 9 && XCord == 0)
+			operationNumber = 9;
+		else if (YCord > 0 && YCord < 9 && XCord > 0 && YCord < size - amountOfUnits)
+			operationNumber = 10;
+		else if (YCord == 9 && XCord == 0)
+			operationNumber = 11;
+		else if (YCord == 9 && XCord > 0 && XCord < size - amountOfUnits)
+			operationNumber = 12;
+		else
+			return false;
+	}
+	switch (operationNumber)
+	{
+	case 1:
+	{
+		
+		break;
+	}
+	default:
+	{
+		cout << "Îøèáêà!\n";
+	}
+	}
+}
 
+bool Field::checkForAvailability(int XCord, int YCord, int amountOfUnits, bool isVertical, int caseNumber)
+{
+	if (YCord != 9)
+	{
+		for (int i = 0; i < amountOfUnits; i++)
+		{
+			if (!isVertical)
+			{
+				if (field[XCord][YCord + i] == SHIP)
+				{
+					return false;
+				}
+				if (caseNumber == 3 || caseNumber == 1)
+				{
+					if (field[XCord++][YCord + i] == SHIP)
+					{
+						return false;
+					}
+				}
+				if (caseNumber == 3 || caseNumber == 2)
+				{
+					if (field[XCord--][YCord + i] == SHIP)
+					{
+						return false;
+					}
+				}
+			}
+			else
+			{
+				if (field[XCord + i][YCord] == SHIP)
+				{
+					return false;
+				}
+				if (caseNumber == 3 || caseNumber == 1)
+				{
+					if (field[XCord + i][YCord++] == SHIP)
+					{
+						return false;
+					}
+				}
+				if (caseNumber == 3 || caseNumber == 2)
+				{
+					if (field[XCord + i][YCord--] == SHIP)
+					{
+						return false;
+					}
+				}
+			}
+		}
+	}
+	else if (XCord == 0)
+	{
+
+	}
+	else if (XCord == 9)
+	{
+
+	}
+	else
+	{
+		if (field[XCord++][YCord--] == SHIP)
+		return true;
+	}
 }
--- Field.h
+++ Field.h
@@ -2,11 +2,14 @@
 class Field
 {
 private: 
+	const int size = 10;
 	char field[10][10];	
+	enum { SHIP = '*', NOTHING = '\0', HIT = '+', MISS = '-' };
 public: 
 	void setUnit(char sign, int XCord, int YCord);
 	char returnUnit(int XCord, int YCord) { return field[XCord][YCord]; };
 	void print();
 	bool isAvailable(int XCord, int YCord, int amountOfUnits, bool isVertical);
+	bool checkForAvailability(int XCord, int YCord, int amountOfUnits, bool isVertical, int caseNumber);
 };
 
